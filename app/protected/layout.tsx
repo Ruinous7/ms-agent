599a7215b86051ad620a5c5d87b8cb2b49b2f62e
Ruinous7 <mizrahidor2323@gmail.com>
2025-03-01 19:05:10 +0200

    maybe mobile fix

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,6 +1,5 @@
 import Sidebar from '@/components/sidebar/sidebar';
 import styles from './layout.module.scss';
-import { Fragment } from 'react';
 
 export default function ProtectedLayout({
   children,
@@ -8,11 +7,11 @@ export default function ProtectedLayout({
   children: React.ReactNode;
 }) {
   return (
-    <Fragment>
+    <>
       <main className={styles.main}>
         {children}
       </main>
       <Sidebar />
-    </Fragment>
+    </>
   );
 } 
